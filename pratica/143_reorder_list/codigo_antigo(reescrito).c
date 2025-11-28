11e2063f96cd2ec306ffb6e9eec312fa81481907
Juyuvi <juniorvicondo@gmail.com>
2025-11-28 19:12:17 -0300

    eu consegui nao adicionar o codigo certo tbm !!!!!!!

--- pratica/143_reorder_list/codigo_antigo(reescrito).c
+++ pratica/143_reorder_list/codigo_antigo(reescrito).c
@@ -1,43 +1,42 @@
 /**
-
  * Definition for singly-linked list.
-
  * struct ListNode {
-
  *     int val;
-
  *     struct ListNode *next;
-
  * };
-
  */
-
 void reorderList(struct ListNode* head) {
-
     if(head == NULL){
-
         return;
-
     }
-
 
     int size = 0;
-
     struct ListNode * curr = head;
 
-
     while (curr->next != NULL){
-
         printf("\n-- %d\n", size);
-
         curr = curr->next;
-
         size++;
-
     }
 
+    struct ListNode * newHead = head;
     
 
+    for(int i = 0; i <= size; i++){
+        curr = head;
 
+        for(int j = 0; j <= size - i; j++){
+            curr = curr->next;
+        }
+
+        if()
+
+        curr->next = head->next;
+        head->next = curr;
+
+        head = head->next->next;
+    }
+
+    head = newHead;
 
 }
