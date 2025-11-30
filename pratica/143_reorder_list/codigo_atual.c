a80e82c5a27cf57afb594c6a5409022a558cc5f6
Juyuvi <juniorvicondo@gmail.com>
2025-11-30 17:15:31 -0300

    doom

--- pratica/143_reorder_list/codigo_atual.c
+++ pratica/143_reorder_list/codigo_atual.c
@@ -1,47 +1,49 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
-void reorderList(struct ListNode* head) {
-    if(head == NULL){
-        return;
-    }
+/*
+====================
+ReorderList
+====================
+*/
+void ReorderList( listNode_t *head ) {
+	if ( !head ) {
+		return;
+	}
 
-    int size = 0;
-    struct ListNode * curr = head;
+	int size = 0;
+	listNode_t *curr = head;
 
-    while (curr->next != NULL){
-        printf("\n-- %d\n", size);
-        curr = curr->next;
-        size++;
-    }
+	while ( curr->next ) {
+		printf( "\n-- %d\n", size );
+		curr = curr->next;
+		size++;
+	}
 
-    curr = head;
-    for (int i = 0; i < size/2; i++) {
-        // encontra o penúltimo (a partir de curr)
-        struct ListNode *prev = curr;
-        while (prev->next && prev->next->next) {
-            prev = prev->next;
+	curr = head;
+	for ( int i = 0; i < size / 2; i++ ) {
+		// encontra o penúltimo (a partir de curr)
+		listNode_t *prev = curr;
+		while ( prev->next && prev->next->next ) {
+			prev = prev->next;
+		}
+
+		listNode_t *last = prev->next;
+		// se o "last" já é o próximo de curr (adjacente), acabou
+		if ( last == curr->next ) {
+            break;
         }
 
-        struct ListNode *last = prev->next;
-        // se o "last" já é o próximo de curr (adjacente), acabou
-        if (last == curr->next) break;
+		listNode_t *next = curr->next;
 
-        struct ListNode *next = curr->next;
+		// remove o último
+		prev->next = NULL;
 
-        // remove o último
-        prev->next = NULL;
+		// insere o último logo após curr
+		last->next = next;
+		curr->next = last;
 
-        // insere o último logo após curr
-        last->next = next;
-        curr->next = last;
-
-        // avança curr duas posições
-        curr = next;
-        if (!curr) break;
-    }
+		// avança curr duas posições
+		curr = next;
+		if ( !curr ) {
+            break;
+        }
+	}
 }
